1e9f292cbfeb8fd5566944e736f4e4716e0f40db
Pedro Correa <thanos@was.right>
2018-05-13 10:16:44 -0300

    Adds string reverse package

--- hello/hello.go
+++ hello/hello.go
@@ -1,7 +1,9 @@
 package main
 
-import "fmt"
+import ("fmt"
+	"github.com/pedroorez/godump/string")
 
 func main() {
 	 fmt.Println("hello, new gopher!")
+	 fmt.Println(string.Reverse("hello, new gopher!"))
 }
