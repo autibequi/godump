4e8d20c8e6d1261873b7423dcb932f1ef6dd203d
Pedro Correa <thanos@was.right>
2018-05-13 10:16:56 -0300

    Adds string reverse test

---
+++ string/string_test.go
@@ -0,0 +1,21 @@
+package string
+
+import "testing"
+
+func Test(t *testing.T) {
+	// Inicia fixtures
+	var tests = []struct {
+		s, want string
+	}{
+		{"Backward", "drawkcaB"},
+		{"😋i", "i😋"},
+	}
+
+	// Implementa de fato os testes
+	for _, c := range tests {
+		got := Reverse(c.s)
+		if got != c.want {
+			t.Errorf("Reverse(%q) == %q, want %q", c.s,got, c.want)
+		}
+	}
+}
