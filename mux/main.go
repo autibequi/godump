87a31977cd55e98ba6aec2bcfb8bedd97e6f8b57
Pedro Correa <thanos@was.right>
2018-05-13 19:04:38 -0300

    Adds basic list books endpoint.

---
+++ mux/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"encoding/json"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+)
+
+// Book Struct (Model)
+type Book struct {
+	ID     string  `json:"id"`
+	Isbn   string  `json:"isbn"`
+	Title  string  `json:"title"`
+	Author *Author `json:"title"`
+}
+
+// Init books var as a slice Book Struct
+var books []Book
+
+// Author Struct
+type Author struct {
+	Firstname string `json:"firstname"`
+	Lastname  string `json:"lastname"`
+}
+
+// Get All Books
+func getBooks(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(books)
+}
+
+func main() {
+	// Init Router
+	r := mux.NewRouter()
+
+	// Mock Data
+	books = append(books, Book{ID: "1", Isbn: "123", Title: "Book one", Author: &Author{Firstname: "John", Lastname: "Doe"}})
+	books = append(books, Book{ID: "2", Isbn: "123123", Title: "Book two", Author: &Author{Firstname: "Johna", Lastname: "Smith"}})
+
+	// Router Handlers/Endpoints
+	r.HandleFunc("/api/books", getBooks).Methods("GET")
+
+	// Start Server
+	log.Fatal(http.ListenAndServe(":8080", r))
+
+}
