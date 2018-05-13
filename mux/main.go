920e563894c22c4564261e96f6e892bb3d54d93f
Pedro Correa <thanos@was.right>
2018-05-13 19:34:00 -0300

    Fix struct json keyname

--- mux/main.go
+++ mux/main.go
@@ -12,7 +12,7 @@ type Book struct {
 	ID     string  `json:"id"`
 	Isbn   string  `json:"isbn"`
 	Title  string  `json:"title"`
-	Author *Author `json:"title"`
+	Author *Author `json:"author"`
 }
 
 // Init books var as a slice Book Struct
