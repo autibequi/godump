818de9fa3bc3014d99e2a4865879ee1f20f09763
pedro.correa@concrete.com.br <pedro.correa@concrete.com.br>
2019-04-12 14:32:15 -0300

    Define mechchan endpoints

--- mehchan/main.go
+++ mehchan/main.go
@@ -39,23 +39,38 @@ func getTrainersCollection() *mongo.Collection {
 var trainers = getTrainersCollection()
 
 func pong(ctx iris.Context) {
+	board := ctx.Params().Get("board")
+	thread := ctx.Params().Get("thread")
+	post := ctx.Params().Get("post")
 
 	ctx2, _ := context.WithTimeout(context.Background(), 5*time.Second)
 	res, _ := trainers.InsertOne(ctx2, bson.M{"name": "pi", "value": 3.14159})
 	id := res.InsertedID
 
 	ctx.JSON(iris.Map{
-		"id": id,
+		"id":    id,
+		"board": board,
+		"thread": thread,
+		"post":  post,
 	})
 }
 
 func main() {
 	app := iris.Default()
-	app.Post("/{board}/{thread}", pong)
-	app.Get("/{board}/{thread}", pong)
+	// Resource Management
+	app.Delete("/{board:string}/{thread:string}/{post:string}", pong) // Delete a resource
+	app.Delete("/{board:string}/{thread:string}", pong)               // Delete a thread
+	app.Post("/{board:string}/{thread:string}", pong)                 // Post to a thread
+	app.Get("/{board:string}/{thread:string}", pong)                  // Get all post of a thread
 
-	app.Get("/{board}", pong)
-	app.Post("/{board}", pong)
+	// Thread Management
+	app.Get("/{board:string}", pong)  // Get all threads of a Board
+	app.Post("/{board:string}", pong) // Post a thread to a Board
+
+	// Board Management
+	app.Delete("/{board:string}", pong) // Delete a board
+	app.Get("/", pong)                  // Get all Boards
+	app.Post("/", pong)                 // Create a Board
 
 	app.Run(iris.Addr(":8080"))
 }
